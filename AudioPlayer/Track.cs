cbf3e4c72b1caed890c44d860429e0c9494f3ce5
KricsDavid <david@kricsfalusi.hu>
2024-05-15 09:55:02 +0200

    55

--- AudioPlayer/Track.cs
+++ AudioPlayer/Track.cs
@@ -8,10 +8,23 @@ namespace AudioPlayer
 {
     public class Track
     {
+        public Track(string fileName, string v1, string v2, string v3, TimeSpan zero)
+        {
+            FileName = fileName;
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+            Zero = zero;
+        }
+
         public string FileName { get; set; }
         public string FriendlyName { get; set; }
         public string Artist { get; set; }
         public string Title { get; set; }
         public TimeSpan Length { get; set; }
+        public string V1 { get; }
+        public string V2 { get; }
+        public string V3 { get; }
+        public TimeSpan Zero { get; }
     }
 }
