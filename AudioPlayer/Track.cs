b6b8902c46b3fc78d4f8fedfb0ae489af8158daa
KricsDavid <david@kricsfalusi.hu>
2024-05-15 10:25:47 +0200

    sdfsf

--- AudioPlayer/Track.cs
+++ AudioPlayer/Track.cs
@@ -6,25 +6,16 @@ using System.Threading.Tasks;
 
 namespace AudioPlayer
 {
-    public class Track
+    public class Track(string fileName, string v1, string v2, string v3, TimeSpan zero)
     {
-        public Track(string fileName, string v1, string v2, string v3, TimeSpan zero)
-        {
-            FileName = fileName;
-            V1 = v1;
-            V2 = v2;
-            V3 = v3;
-            Zero = zero;
-        }
-
-        public string FileName { get; set; }
+        public string FileName { get; set; } = fileName;
         public string FriendlyName { get; set; }
         public string Artist { get; set; }
         public string Title { get; set; }
         public TimeSpan Length { get; set; }
-        public string V1 { get; }
-        public string V2 { get; }
-        public string V3 { get; }
-        public TimeSpan Zero { get; }
+        public string V1 { get; } = v1;
+        public string V2 { get; } = v2;
+        public string V3 { get; } = v3;
+        public TimeSpan Zero { get; } = zero;
     }
 }
